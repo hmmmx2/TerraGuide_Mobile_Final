c1ec0d398b2b87111d01ec669e591cca4d37ead8
ZN1DEX <146168607+ZN1DEX@users.noreply.github.com>
2025-05-31 10:32:20 +0800

    Initial commit for final repo

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from 'expo-router';
 import '../global.css';
 import { AuthProvider } from '@/context/AuthProvider';
+import { UserManagementProvider } from '@/context/UserManagementProvider';
 import { ToastContainer } from '@/components/CustomToast';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
@@ -38,173 +39,117 @@ export default function RootLayout() {
 
     return (
         <AuthProvider>
-            <StatusBar style='light' backgroundColor='#4E6E4E' translucent={false}/>
-            <Stack
-                screenOptions={{
-                    headerShown: false,
-                    animation: 'fade',
-                    contentStyle: { backgroundColor: '#FFFFFF' },
-                }}
-            >
-                {/* Initial & Auth Screens */}
-                <Stack.Screen name='index' options={{ animation: 'none' }} />
-                <Stack.Screen name='OnboardingScreen' options={{ animation: 'slide_from_right' }} />
-                <Stack.Screen name='LoginScreen' />
-                <Stack.Screen name='RegistrationScreen' />
-                <Stack.Screen name='ForgotPasswordScreen' />
-                <Stack.Screen name='VerifyCodeScreen' />
-                <Stack.Screen name='ChangePasswordScreen' />
+            <UserManagementProvider>
+                <StatusBar style='light' backgroundColor='#4E6E4E' />
+                <Stack
+                    screenOptions={{
+                        headerShown: false,
+                        animation: 'fade',
+                        contentStyle: { backgroundColor: '#FFFFFF' },
+                    }}
+                >
+                    {/* Initial & Auth Screens */}
+                    <Stack.Screen name='index' options={{ animation: 'none' }} />
+                    <Stack.Screen name='OnboardingScreen' options={{ animation: 'slide_from_right' }} />
+                    <Stack.Screen name='LoginScreen' />
+                    <Stack.Screen name='RegistrationScreen' />
+                    <Stack.Screen name='ForgotPasswordScreen' />
+                    <Stack.Screen name='VerifyCodeScreen' />
+                    <Stack.Screen name='ChangePasswordScreen' />
 
-                {/* Payment Screens */}
-                <Stack.Screen name='PaymentScreen' />
-                <Stack.Screen name='PaymentSuccessScreen' />
+                    {/* Payment Screens */}
+                    <Stack.Screen name='PaymentScreen' />
+                    <Stack.Screen name='PaymentSuccessScreen' />
 
-                {/* Main Dashboard Screens - prevent back navigation */}
-                <Stack.Screen name='HomeGuestScreen' options={{ gestureEnabled: false }} />
-                <Stack.Screen name='HomeParkGuideScreen' options={{ gestureEnabled: false }} />
-                <Stack.Screen name='DashboardScreen' options={{ gestureEnabled: false }} />
+                    {/* Main Dashboard Screens */}
+                    <Stack.Screen name='HomeGuestScreen' options={{ gestureEnabled: false }} />
+                    <Stack.Screen name='HomeParkGuideScreen' options={{ gestureEnabled: false }} />
+                    <Stack.Screen name='DashboardScreen' options={{ gestureEnabled: false }} />
 
-                {/* Profile Screens */}
-                <Stack.Screen name='ProfileScreen' />
-                <Stack.Screen name='GuestProfileScreen' />
-                <Stack.Screen name='EditProfileScreen' />
-                
-                {/* Content & Feature Screens */}
-                <Stack.Screen name='SurveyFormScreen' />
-                <Stack.Screen name='BlogDetailScreen' />
-                <Stack.Screen name='TimetableScreen' />
-                <Stack.Screen name='CourseScreen' />
-                <Stack.Screen name='CourseDetails' />
-                <Stack.Screen name='GuideDetailScreen' />
-                <Stack.Screen name='DextAIScreen' />
-                <Stack.Screen name='RecommendationScreen' />
-                <Stack.Screen name='FloraFaunaScreen' />
-                <Stack.Screen name='LicenseScreen' />
-                <Stack.Screen name='LessonVideoScreen' />
-                <Stack.Screen name='AssessmentDescription' />
-                <Stack.Screen name='AssessmentQuestion' />
-                <Stack.Screen name='AssessmentResultScreen' />
-                <Stack.Screen name='LicenseDetailsScreen' />
-                <Stack.Screen name='BlogsScreen' />
-                <Stack.Screen name='InteractiveMap' />
-                <Stack.Screen name='OtherParkGuideScreen' />
-                <Stack.Screen name='NotificationScreen' />
+                    {/* Profile Screens */}
+                    <Stack.Screen name='ProfileScreen' />
+                    <Stack.Screen name='GuestProfileScreen' />
+                    <Stack.Screen name='EditProfileScreen' />
 
-                {/* QR & Survey Features */}
-                <Stack.Screen
-                    name='QRSurveyScreen'
-                    options={{
-                        animation: 'slide_from_right',
-                        presentation: 'modal'
-                    }}
-                />
-                <Stack.Screen
-                    name='QRScannerScreen'
-                    options={{
-                        animation: 'slide_from_right',
-                        presentation: 'fullScreenModal'
-                    }}
-                />
-                <Stack.Screen
-                    name='GuestSurveyFormScreen'
-                    options={{
-                        animation: 'slide_from_bottom',
-                        gestureEnabled: true
-                    }}
-                />
+                    {/* Course & Learning Screens */}
+                    <Stack.Screen name='CourseScreen' />
+                    <Stack.Screen name='CourseDetails' />
+                    <Stack.Screen name='CourseSurveyFormScreen' />
+                    <Stack.Screen name='CourseDetailsScreen' />
+                    <Stack.Screen name='OnlineCourseScreen' />
+                    <Stack.Screen name='MentorProgrammeScreen' />
+                    <Stack.Screen name='LessonVideoScreen' />
+                    <Stack.Screen name='TimetableScreen' />
 
-                {/* Admin Management Screens */}
-                <Stack.Screen
-                    name="DatabaseScreen"
-                    options={{
-                        animation: 'slide_from_right',
-                        gestureEnabled: true
-                    }}
-                />
-                <Stack.Screen
-                    name="RoleManagementScreen"
-                    options={{
-                        animation: 'slide_from_right',
-                        gestureEnabled: true
-                    }}
-                />
-                <Stack.Screen
-                    name="LicenseManagementScreen"
-                    options={{
-                        animation: 'slide_from_right',
-                        gestureEnabled: true
-                    }}
-                />
+                    {/* Assessment Screens */}
+                    <Stack.Screen name='AssessmentDescription' />
+                    <Stack.Screen name='AssessmentQuestion' />
+                    <Stack.Screen name='AssessmentResultScreen' />
 
-                {/* Content Management Screens */}
-                <Stack.Screen
-                    name="ContentManagementScreen"
-                    options={{
-                        animation: 'slide_from_right',
-                        gestureEnabled: true
-                    }}
-                />
-                <Stack.Screen
-                    name="CourseManagementScreen"
-                    options={{
-                        animation: 'slide_from_right',
-                        gestureEnabled: true
-                    }}
-                />
-                <Stack.Screen
-                    name="MentorProgrammeManagementScreen"
-                    options={{
-                        animation: 'slide_from_right',
-                        gestureEnabled: true
-                    }}
-                />
-                <Stack.Screen
-                    name="RecommendedCourseManagementScreen"
-                    options={{
-                        animation: 'slide_from_right',
-                        gestureEnabled: true
-                    }}
-                />
+                    {/* License Screens */}
+                    <Stack.Screen name='LicenseScreen' />
+                    <Stack.Screen name='LicenseDetailsScreen' />
 
-                {/* Add/Create Screens */}
-                <Stack.Screen
-                    name="AddCourseScreen"
-                    options={{
-                        animation: 'slide_from_bottom',
-                        presentation: 'modal',
-                        gestureEnabled: true
-                    }}
-                />
-                <Stack.Screen
-                    name="AddMentorProgrammeScreen"
-                    options={{
-                        animation: 'slide_from_bottom',
-                        presentation: 'modal',
-                        gestureEnabled: true
-                    }}
-                />
-                <Stack.Screen
-                    name="AddRecommendedCourseScreen"
-                    options={{
-                        animation: 'slide_from_bottom',
-                        presentation: 'modal',
-                        gestureEnabled: true
-                    }}
-                />
-                <Stack.Screen
-                    name="AddNewUserScreen"
-                    options={{
-                        animation: 'slide_from_bottom',
-                        presentation: 'modal',
-                        gestureEnabled: true
-                    }}
-                />
+                    {/* Content & Info Screens */}
+                    <Stack.Screen name='BlogsScreen' />
+                    <Stack.Screen name='BlogDetailScreen' />
+                    <Stack.Screen name='GuideDetailScreen' />
+                    <Stack.Screen name='RecommendationScreen' />
+                    <Stack.Screen name='FloraFaunaScreen' />
+                    <Stack.Screen name='InteractiveMap' />
+                    <Stack.Screen name='OtherParkGuideScreen' />
+                    <Stack.Screen name='NotificationScreen' />
 
-                {/* Test Screens */}
-                <Stack.Screen name='ToastTestScreen' />
-            </Stack>
-            {/*<SessionExpiryWarning />*/}
-            <ToastContainer />
+                    {/* AI & Features */}
+                    <Stack.Screen name='DextAIScreen' />
+
+                    {/* QR & Survey */}
+                    <Stack.Screen
+                        name='QRSurveyScreen'
+                        options={{
+                            animation: 'slide_from_right',
+                            presentation: 'modal'
+                        }}
+                    />
+                    <Stack.Screen
+                        name='QRScannerScreen'
+                        options={{
+                            animation: 'slide_from_right',
+                            presentation: 'fullScreenModal'
+                        }}
+                    />
+                    <Stack.Screen
+                        name='GuestSurveyFormScreen'
+                        options={{
+                            animation: 'slide_from_bottom',
+                            gestureEnabled: true
+                        }}
+                    />
+                    <Stack.Screen name='SurveyFormScreen' />
+
+                    {/* Admin Management Screens */}
+                    <Stack.Screen name='DatabaseScreen' options={{ animation: 'slide_from_right', gestureEnabled: true }} />
+                    <Stack.Screen name='RoleManagementScreen' options={{ animation: 'slide_from_right', gestureEnabled: true }} />
+                    <Stack.Screen name='LicenseManagementScreen' options={{ animation: 'slide_from_right', gestureEnabled: true }} />
+
+                    {/* Content Management */}
+                    <Stack.Screen name='ContentManagementScreen' options={{ animation: 'slide_from_right', gestureEnabled: true }} />
+                    <Stack.Screen name='CourseManagementScreen' options={{ animation: 'slide_from_right', gestureEnabled: true }} />
+                    <Stack.Screen name='MentorProgrammeManagementScreen' options={{ animation: 'slide_from_right', gestureEnabled: true }} />
+                    <Stack.Screen name='RecommendedCourseManagementScreen' options={{ animation: 'slide_from_right', gestureEnabled: true }} />
+
+                    {/* Add/Create */}
+                    <Stack.Screen name='AddCourseScreen' options={{ animation: 'slide_from_bottom', presentation: 'modal', gestureEnabled: true }} />
+                    <Stack.Screen name='AddMentorProgrammeScreen' options={{ animation: 'slide_from_bottom', presentation: 'modal', gestureEnabled: true }} />
+                    <Stack.Screen name='AddRecommendedCourseScreen' options={{ animation: 'slide_from_bottom', presentation: 'modal', gestureEnabled: true }} />
+                    <Stack.Screen name='AddNewUserScreen' options={{ animation: 'slide_from_bottom', presentation: 'modal', gestureEnabled: true }} />
+
+                    {/* Test */}
+                    <Stack.Screen name='ToastTestScreen' />
+                </Stack>
+                {/* <SessionExpiryWarning /> */}
+                <ToastContainer />
+            </UserManagementProvider>
         </AuthProvider>
     );
 }
